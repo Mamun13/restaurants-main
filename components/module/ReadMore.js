024187703e71b206fa314a47acc4f758762905dd
Mamun13 <mamuncse82@gmail.com>
2024-01-25 17:42:41 +0600

    update res

--- components/module/ReadMore.js
+++ components/module/ReadMore.js
@@ -5,7 +5,7 @@ import { Container } from 'react-bootstrap';
 const ReadMore = () => {
   return (
     <>
-      <section className='read_more'>
+      <section className='read_more_part'>
         <div className='read_more_banner position-relative'>
           <img
             src='/assets/images/read_more.png'
@@ -14,19 +14,21 @@ const ReadMore = () => {
           />
         </div>
         <Container>
-          <h1 className='text-center mt-4 lh-1 px-3 pb-3'> 
-            Arrowhead Grill <br/> Experience the art of authentic meat platter
-            mastery.
-          </h1>
-          <p className='px-3 text-justify read_more_des'>
-            Indulge in the culinary excellence of Arrowhead Grill, where the art
-            of authentic meat platter mastery comes to life. Immerse yourself in
-            a symphony of flavors as our skilled chefs craft a tantalizing array
-            of premium cuts, expertly grilled to perfection. From succulent
-            steaks to savory chops, each bite is a celebration of taste and
-            texture. Elevate your dining experience at Arrowhead Grill, where
-            passion for quality meets the mastery of meat.
-          </p>
+          <div className='read_more_padding'>
+            <h1 className='text-center mt-4 lh-1 px-3 pb-3 fw_400'>
+              Arrowhead Grill <br /> Experience the art of authentic meat
+              platter mastery.
+            </h1>
+            <p className='px-3 read_more_des pb-5'>
+              Indulge in the culinary excellence of Arrowhead Grill, where the
+              art of authentic meat platter mastery comes to life. Immerse
+              yourself in a symphony of flavors as our skilled chefs craft a
+              tantalizing array of premium cuts, expertly grilled to perfection.
+              From succulent steaks to savory chops, each bite is a celebration
+              of taste and texture. Elevate your dining experience at Arrowhead
+              Grill, where passion for quality meets the mastery of meat.
+            </p>
+          </div>
         </Container>
       </section>
     </>
